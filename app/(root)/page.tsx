fc4d7beea11420c838cde8ed299dd94f19641004
chinturai <chintu.23ise@cambridge.edu.in>
2025-03-15 12:37:37 +0530

    Finally Error sorted

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import SearchForm from "../components/SearchForm";
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading"> <p className="text-8xl">HustleHive</p> Pitch your startups <br /> to potential Investors </h1>
+        <p className="sub-heading !max-w-3xl"> 
+          Submit your ideas, Vote on pitches, and take part in virtual competitions ! 
+        </p>
+
+        <SearchForm/>
+      </section>
+    </>  
+  );
+}
