aad003faf1b2740e45da11d2dcc1038283ff34ca
chinturai <chintu.23ise@cambridge.edu.in>
2025-03-31 09:32:17 +0530

    Back to Dev after INTERNAL EXAMS

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,31 @@
+import Form from 'next/form'
+import ResetButton from './ResetButton';
 
 const SearchForm = () => {
+  const query = "test";
+
+  
+
   return (
-    <div>SearchForm</div>
+    <Form action="/" scroll={false} className='search-form'>
+      <input 
+      name='query'
+      defaultValue={query}
+      className='search-input'
+      placeholder='Search for Start-ups'
+      />
+
+      <div className='flex gap-2'>
+        { query && (
+          <ResetButton />
+        )}
+
+        <button type='submit' className='search-btn'>
+          🔍
+        </button>
+      </div>
+    </Form>
   )
-}
+} 
 
 export default SearchForm
