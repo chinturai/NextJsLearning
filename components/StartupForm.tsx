33e43f7a87baf0f906bbcd14157b08c37e192d8c
chinturai <chintu.23ise@cambridge.edu.in>
2025-04-04 23:10:48 +0530

    4h30mins done

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,13 +1,56 @@
-'use-client';
+"use client";
 
-import { useState } from "react";
+import { useActionState, useState } from "react";
 import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import MDEditor from '@uiw/react-md-editor';
+import { Button } from "./ui/button";
+import { formSchema } from "@/lib/validation";
+import { z } from 'zod';
+import { useRouter } from "next/navigation";
+
 
 const StartupForm = () => {
 
-  const [errors, setErrors] = useState<Record<string,string>>({});
+  const router = useRouter();
 
-  return <form className="startup-form">
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const [pitch, setPitch] = useState("**Bring your ideas infront of the world with, HustleHive**");
+
+  const handleFormSubmit = async (prevState : any , formData : FormData)=>{
+    try {
+      const formValues = {
+        title : formData.get("title") as string,
+        description : formData.get("description") as string,
+        category : formData.get("category") as string,
+        link : formData.get("link") as string,
+        pitch
+      }
+
+      await formSchema.parseAsync(formValues);
+
+      // const result = await createIdea(prevState , formData , pitch);
+
+      // router.push(`/startup/${result.id}`);
+    } catch (error) {
+      if(error instanceof z.ZodError){
+        const fieldErrors = error.flatten().fieldErrors;
+
+        setErrors(fieldErrors as unknown as Record<string , string>);
+
+        return { ...prevState , error: "Validation Failed" , status: "ERROR"}
+      }
+
+      return { ...prevState , error: "Unexpected error occured", status: "ERROR"}
+    }
+  };
+
+  const [state , formAction, isPending] = useActionState(handleFormSubmit , {error: "" , status: "INITIAL"});
+
+
+  return <form action={formAction} className="startup-form">
+    {/* Title Input */}
     <div>
       <label htmlFor="title" className="startup-form_label"> Title </label>
       <Input
@@ -18,8 +61,63 @@ const StartupForm = () => {
       />
 
       {errors.title && <p className="startup-form_label">{errors.title}</p>}
-
     </div>
+
+    {/* Description Input */}
+    <div>
+      <label htmlFor="description" className="startup-form_label"> description </label>
+      <Textarea
+        id="description"
+        name="description"
+        className="startup-form_textarea"
+        required placeholder="Describe your Startup in/around 100 words"
+      />
+
+      {errors.description && <p className="startup-form_label">{errors.description}</p>}
+    </div>
+
+    {/* Category Input */}
+    <div>
+      <label htmlFor="category" className="startup-form_label"> Category </label>
+      <Input
+        id="category"
+        name="category"
+        className="startup-form_input"
+        required placeholder="Select the category that represents your startup the best"
+      />
+
+      {errors.category && <p className="startup-form_label">{errors.category}</p>}
+    </div>
+
+    {/* Image URL Input */}
+    <div>
+      <label htmlFor="link" className="startup-form_label"> Image URL </label>
+      <Input
+        id="link"
+        name="link"
+        className="startup-form_input"
+        required placeholder="Any relevant images you wanna showcase in your pitch"
+      />
+
+      {errors.link && <p className="startup-form_label">{errors.link}</p>}
+    </div>
+
+    {/* Markdown Editor */}
+    <div data-color-mode="light">
+      <label htmlFor="Pitch" className="startup-form_label"> Pitch </label>
+      <MDEditor
+        value={pitch}
+        onChange={(value) => setPitch(value as string)}
+        height={350}
+        style={{ borderRadius: 30, overflow: "hidden" }}
+      />
+      {errors.pitch && <p className="startup-form_label">{errors.pitch}</p>}
+    </div>
+
+    {/* Submit Button */}
+    <Button type="submit" className="startup-form_btn text-white font-bold" disabled={isPending} >
+      {isPending ? `Submittinng...` : `Submit your Pitch`} 
+    </Button>
   </form>
 }
 
